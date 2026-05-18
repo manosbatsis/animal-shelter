099dafed5d0a65890e530056db85be9778122dfb
Daniel Garcia <albdangar@gmail.com>
2026-05-18 01:36:17 -0400

    chore: upgrade to nextjs 16

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,8 @@
 import NextAuth from 'next-auth';
-import { NextRequest } from 'next/server';
 import { authProviderConfigList } from './auth.config';
 
-const { auth } = NextAuth(authProviderConfigList)
-export default auth(async function middleware(req: NextRequest) {
-  // custom middleware logic goes here
-})
+const { auth: proxy } = NextAuth(authProviderConfigList)
+export default proxy;
 
 // Filter Middleware to avoid API routes and static assets
 export const config = {
